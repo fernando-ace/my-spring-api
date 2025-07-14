d0ca8a73e1c0222f1c1fb61981569f7b5dd62ae4
Fernando Acevedo <FernandoJosueAcevedo@gmail.com>
2025-07-14 17:23:33 +0000

    Delete unnecessary files

--- src/main/java/com/fernando/restapi/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fernando.restapi.repository;
-
-import com.fernando.restapi.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    // Additional query methods if needed
-}
