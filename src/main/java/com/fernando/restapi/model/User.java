2bd89b7375a2d455445210cb9338cbd00c974d3a
Fernando Acevedo <FernandoJosueAcevedo@gmail.com>
2025-07-21 09:42:25 -0500

    Rename User table to app_user to avoid reserved word issues

--- src/main/java/com/fernando/restapi/model/User.java
+++ src/main/java/com/fernando/restapi/model/User.java
@@ -1,12 +1,9 @@
 package com.fernando.restapi.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
